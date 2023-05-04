b3c85cba5924f13b122125932ac4e3b9e17d9404
przekaska <kacper.blaszkowski.12@gmail.com>
2023-05-04 21:55:27 +0200

    Deleted name and createchild()

--- src/simple-tree.cpp
+++ src/simple-tree.cpp
@@ -7,22 +7,10 @@
 
 class Node{
     public:
-    std::string name;
     Node *parent;
     std::list <Node*> children;
 
-    Node(std::string new_name){
-        name = new_name;
-    }
-
     Node addchild(Node &child){
-        child.parent = this;
-        children.push_back(&child);
-        return child;
-    }
-
-    Node createchild(std::string name){
-        Node child(name);
         child.parent = this;
         children.push_back(&child);
         return child;
