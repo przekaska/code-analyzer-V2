6838afa2a2a2ac8827b598990b7503bcbd5685be
przekaska <kacper.blaszkowski.12@gmail.com>
2023-05-06 10:04:51 +0200

    Created function 'find_funcs'

--- src/code-analyzer.cpp
+++ src/code-analyzer.cpp
@@ -8,13 +8,13 @@
 #include <vector>
 
 #include "simple-tree.cpp"
-#include "scopes.cpp"
-
+#include "functions.cpp"
 
 #define FUNC_DECOR_REGEX "(void|int|float)+(\\*)?\\s+(\\*)?\\w+(\\s+)?\\(((\\s?(void|int|float)+(\\*)?\\s+(\\*)?\\w+(\\s+)?,?\\s?)?)+\\)"
 #define DECOR_TYPE_REGEX "(void|int|float)"
 #define DECOR_NAME_REGEX "\\w+\\s?(?=\\()"
 
+std::vector <Func> funcs;
 
 void readfile(const char *filename, std::string &text_buffer){
     std::ifstream source_file(filename);
@@ -42,6 +42,17 @@ int find_func_end(std::string &text){
             return &ch - &text[0];
     }
     return -1;
+}
+
+void find_funcs(std::string text){ 
+    std::smatch match;
+    std::regex func_decor_expr(FUNC_DECOR_REGEX);
+
+    while (regex_search(text, match, func_decor_expr)){
+        funcs.push_back(Func(find_in(match[0], DECOR_NAME_REGEX), find_in(match[0], DECOR_TYPE_REGEX)));
+        text = text.substr(match.position() + match.length());
+        funcs.back().code = text.substr(1, find_func_end(text) - 2);
+    }
 }
 
 int main(){
